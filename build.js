c896111847b08f12c4d16e8d76e964a3fd269d52
Steve Washington <washington.steven@gmail.com>
2021-11-02 22:49:02 -0400

    Netlify build maybe

--- build.js
+++ build.js
@@ -2,17 +2,7 @@ const webpack = require("webpack");
 const config = require("./webpack.config");
 const { exec } = require("child_process");
 
-exec("which tweego", (error, stdout, stderr) => {
-    if (error) {
-        console.error(`Error running \`which tweego\`: ${error.message}`);
-        console.error("Did you remember to install Tweego? (https://www.motoslave.net/tweego/)");
-        return;
-    }
-    if (stderr) {
-        console.error(`stderr (\`which tweego\`): ${stderr}`);
-        return;
-    }
-
+const runBuild = (tweegoExecutable) => {
     webpack(config, (err, stats) => {
         if (err) {
             throw err;
@@ -22,7 +12,7 @@ exec("which tweego", (error, stdout, stderr) => {
             return;
         }
 
-        const tweegoCommand = `tweego -o ${__dirname}/build/index.html -m ${__dirname}/dist --log-stats ${__dirname}/story`
+        const tweegoCommand = `${tweegoExecutable} -o ${__dirname}/build/index.html -m ${__dirname}/dist --log-stats ${__dirname}/story`
         console.log(tweegoCommand)
         exec(tweegoCommand, (error, stdout, stderr) => {
             if (error) {
@@ -35,4 +25,34 @@ exec("which tweego", (error, stdout, stderr) => {
             console.log("...done.");
         });
     });
-});
+}
+
+if (process.env.NETLIFY) {
+    exec(`${path.resolve(__dirname, "bin", "get-tweego")}`, (error, stdout, stderr) => {
+        if (error) {
+            console.error("Error getting tweego", error);
+            return;
+        }
+        if (stderr) {
+            console.log("stderr", stderr);
+        }
+        if (stdout) {
+            console.log("stdout", stdout);
+        }
+        runBuild(path.resolve(__dirname, "tweego", "tweego"));
+    })
+} else {
+    exec("which tweego", (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error running \`which tweego\`: ${error.message}`);
+            console.error("Did you remember to install Tweego? (https://www.motoslave.net/tweego/)");
+            return;
+        }
+        if (stderr) {
+            console.error(`stderr (\`which tweego\`): ${stderr}`);
+            return;
+        }
+    
+        runBuild("tweego");
+    });
+}
